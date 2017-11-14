2abdc1a03eb231c49b2645103efe5e702ba206cc
Valentina <valentina.rmo@gmail.com>
2017-11-14 11:54:35 +0000

    Adding test for multiple of 5 and for multiple of 3 & 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,10 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
       expect(3.fizzbuzz).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+  it 'return "fizzbuzz" when pass 15' do
+    expect(15.fizzbuzz).to eq "fizzbuzz"
+  end
 end
