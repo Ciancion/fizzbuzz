34b8954ceed81013622907041d0c7089ec440788
Valentina <valentina.rmo@gmail.com>
2017-11-14 14:17:19 +0000

    Adding extra test for new conditions

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,4 +9,13 @@ describe 'fizzbuzz' do
   it 'return "fizzbuzz" when pass 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+  it 'return "one" when pass 1' do
+    expect(fizzbuzz(1)).to eq "one"
+  end
+  it 'return "sixty" when pass 60' do
+    expect(fizzbuzz(60)).to eq "sixty"
+  end
+  it 'return "ninty-nine" when pass 99' do
+    expect(fizzbuzz(99)).to eq "ninty-nine"
+  end
 end
