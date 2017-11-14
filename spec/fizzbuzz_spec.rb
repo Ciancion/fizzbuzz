4e782b26023c12fc55833ae57da13342425143a0
Valentina <valentina.rmo@gmail.com>
2017-11-14 13:13:32 +0000

    add more tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,12 +1,12 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-      expect(3.fizzbuzz).to eq 'fizz'
+      expect(fizzbuzz(3)).to eq 'fizz'
   end
   it 'returns "buzz" when passed 5' do
-    expect(5.fizzbuzz).to eq 'buzz'
+    expect(fizzbuzz(5)).to eq 'buzz'
   end
   it 'return "fizzbuzz" when pass 15' do
-    expect(15.fizzbuzz).to eq "fizzbuzz"
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
