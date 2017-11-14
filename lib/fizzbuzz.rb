c3c5c23aa9ac8fc964f2f3edfdda426e36189e61
Valentina <valentina.rmo@gmail.com>
2017-11-14 14:16:00 +0000

    Adding extra conditions in fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,9 @@
 def fizzbuzz(number)
-    return "fizzbuzz" if number % 3 == 0 && number%5 == 0
-    return "fizz" if number % 3 == 0
-    return "buzz" if number % 5 == 0
-    number.to_s
-    end
+  return "one" if number == 1
+  return "sixty" if number == 60
+  return "ninty-nine" if number == 99
+  return "fizzbuzz" if number % 3 == 0 && number%5 == 0
+  return "fizz" if number % 3 == 0
+  return "buzz" if number % 5 == 0
+  number.to_s
+end
